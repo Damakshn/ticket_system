f4792bd848934ea4532d719f8b5c26561a615dcb
Damakshn <damakshon@gmail.com>
2020-03-23 15:47:45 +0800

    Приделываем bootstrap Подключаем crispy forms

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+
 from . import models
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 
 class LoginForm(forms.Form):
@@ -15,6 +18,24 @@ class TicketForm(forms.ModelForm):
         widgets = {
             "deadline": forms.DateInput(attrs={"type": "date"})
         }
+    
+    def __init__(self, *args, **kwargs):
+        super(). __init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.form_action = ""
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                'departament',
+                'title',
+                'description',
+                'deadline'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Сохранить')
+            )
+        )
 
 
 class ExecutorAssignmentForm(forms.Form):
@@ -28,3 +49,83 @@ class ExecutorAssignmentForm(forms.Form):
         # ToDo узнать, почему переопределение метода не сработало
         # https://docs.djangoproject.com/en/3.0/ref/forms/fields/#modelchoicefield
         self.fields["executor"].label_from_instance = lambda obj: f"{obj.last_name} {obj.first_name}"
+        #----------------------------------------------------------
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.form_action = "----calculated dynamically----"
+        self.helper.form_class = "form-inline"
+        self.helper.field_template = 'bootstrap4/layout/inline_field.html'
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                'executor'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Назначить')
+            )
+        )
+
+class SimpleInlineForm(forms.Form):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.form_action = "----calculated dynamically----"
+        self.helper.form_class = "form-inline"
+        self.helper.field_template = 'bootstrap4/layout/inline_field.html'
+
+
+class DelayTicketForm(SimpleInlineForm):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper.layout = Layout(
+            ButtonHolder(
+                Submit('submit', 'Отложить', css_class="btn btn-secondary")
+            )
+        )
+
+
+class DenyTicketForm(SimpleInlineForm):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper.layout = Layout(
+            ButtonHolder(
+                Submit('submit', 'Отклонить', css_class="btn btn-danger")
+            )
+        )
+
+
+class RefreshTicketForm(SimpleInlineForm):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper.layout = Layout(
+            ButtonHolder(
+                Submit('submit', 'Возобновить', css_class="btn btn-success")
+            )
+        )
+
+
+class SetDoneTicketForm(SimpleInlineForm):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper.layout = Layout(
+            ButtonHolder(
+                Submit('submit', 'Готово', css_class="btn btn-success")
+            )
+        )
+
+
+class CompleteTicketForm(SimpleInlineForm):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.helper.layout = Layout(
+            ButtonHolder(
+                Submit('submit', 'Отложить', css_class="btn btn-success")
+            )
+        )
